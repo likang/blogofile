4ffd8ea8c99e36f4f15833178cd3d47e783116d3
likang <likang@kisbear.com>
2012-01-06 15:16:40 +0800

    more readable

--- _new.sh
+++ _new.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-if [ $# -ne 1 ]
+if [ $# -eq 0 ]
 then
-  echo "No post title! usage: _new.sh 'the title'"
+  echo "No post title! Usage: _new.sh 'the title'"
   exit 1;
 fi
 
 mkdir -p _posts
 filepath=_posts/`date +%G-%m-%d`-${1// /-}.markdown
-echo "---\ntitle: $1 \ndate: `date '+%G/%m/%d %H:%M:%S'`\ncategories:\n---" \
-  >> $filepath
+
+echo "\
+---
+title: $1 
+date: `date '+%G/%m/%d %H:%M:%S'`
+categories:
+---" >> $filepath
+
 echo "new post: $filepath"
